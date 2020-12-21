b92532af983f8ba27722470b6c9445b1b15897b5
beezee <brian.zeligson@gmail.com>
2020-12-20 23:40:05 -0500

    add sum type, exercise against ts-adt

---
+++ packages/core/src/index.spec.ts
@@ -0,0 +1,46 @@
+import { makeMatchers } from 'ts-adt/MakeADT';
+import test from "ava";
+import * as lib from './index';
+
+type Ops = "sum" | "str" | "bool" | "num" | "nullable" | "array" | "recurse" | "dict"
+
+type Alg<F extends lib.Target> = lib.Alg<F, Ops>
+
+const t1Props = <F extends lib.Target>(T: Alg<F>) => ({
+  foo: T.nullable({ of: T.str({GraphQL: {type: "ID"}}) }),
+  bar: T.num({GraphQL: {type: "Float"}}),
+  baz: T.nullable({ of: T.bool({}) }),
+});
+
+const t2Props = <F extends lib.Target>(T: Alg<F>) => ({
+  bar: T.nullable({ of: T.str({GraphQL: {type: "ID"}}) }),
+  baz: T.num({GraphQL: {type: "Float"}}),
+  quux: T.nullable({ of: T.bool({}) }),
+});
+
+const t3Props = <F extends lib.Target>(T: Alg<F>) => ({
+  baz: T.nullable({ of: T.str({GraphQL: {type: "ID"}}) }),
+  quux: T.num({GraphQL: {type: "Float"}}),
+  foo: T.nullable({ of: T.bool({}) }),
+});
+
+const adt = <F extends lib.Target>(T: Alg<F>) =>
+  T.sum("type", {t1: t1Props(T), t2: t2Props(T), t3: t3Props(T)})
+
+const assertAdt = adt(lib.Type)
+type ADTM = lib.TypeOf<typeof assertAdt>
+type ADT = ADTM[keyof ADTM]
+const takeAdt = (x: ADT): void => undefined
+takeAdt({foo: "hi", bar: 3, baz: true, type: "t1"})
+const takeT2 = (x: ADTM["t2"]): void => undefined
+takeT2({bar: null, baz: 2, quux: false, type: "t2"})
+const matchI = makeMatchers("type")[2]
+const x: string | null = matchI<ADT>(
+  {bar: null, baz: 2, quux: false, type: "t2"})(
+  {t1: (x) => x.foo, t2: (x) => x.bar, t3: (x) => x.baz})
+
+test("test", async t => {
+  t.true(true)
+});
+
+
